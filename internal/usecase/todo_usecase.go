5214801e94c0c409c9694782b73a7d4aa7716ce1
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-05-22 22:35:40 +0530

    style: Moved repo to new file

--- internal/usecase/todo_usecase.go
+++ internal/usecase/todo_usecase.go
@@ -7,14 +7,6 @@ import (
 	"github.com/ShivangSrivastava/cli-todo/internal/entity"
 )
 
-type TodoRepository interface {
-	GetTodos() ([]entity.Todo, error)
-	CreateTodo(title string) (entity.Todo, error)
-	UpdateTodoTitle(id int, title string) (entity.Todo, error)
-	UpdateTodoIsCompleted(id int, isCompleted bool) (entity.Todo, error)
-	DeleteTodo(id int) error
-}
-
 type TodoUsecase struct {
 	repo TodoRepository
 }
