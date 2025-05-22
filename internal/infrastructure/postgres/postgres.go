ddf4083c04e54e7183899ccbda0c85080469c653
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-05-22 23:00:21 +0530

    feat: Added postgres

--- internal/infrastructure/postgres/postgres.go
+++ internal/infrastructure/postgres/postgres.go
@@ -1,14 +1,14 @@
-package db
+package postgres
 
 import (
 	"database/sql"
 	"log"
 
-	"github.com/ShivangSrivastava/cli-todo/internal/models"
+	"github.com/ShivangSrivastava/cli-todo/internal/entity"
 	_ "github.com/lib/pq"
 )
 
-type DBRepo struct {
+type TodoRepo struct {
 	DB *sql.DB
 }
 
@@ -16,42 +16,41 @@ func createTable(db *sql.DB) error {
 	query := `CREATE TABLE IF NOT EXISTS todos(
 		id SERIAL PRIMARY KEY,
 		title VARCHAR(255) NOT NULL,
-		description VARCHAR(255),
 		is_completed BOOLEAN NOT NULL DEFAULT false
 	)`
 	_, err := db.Exec(query)
 	return err
 }
 
-func NewDBRepo(db *sql.DB) *DBRepo {
+func NewDBRepo(db *sql.DB) *TodoRepo {
 	err := createTable(db)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return &DBRepo{
+	return &TodoRepo{
 		DB: db,
 	}
 }
 
 // Create a todo
-func (r *DBRepo) CreateTodo(title, description string) error {
-	query := `INSERT INTO todos (title, description) VALUES ($1, $2)`
-	_, err := r.DB.Exec(query, title, description)
+func (r *TodoRepo) CreateTodo(title string) error {
+	query := `INSERT INTO todos (title) VALUES ($1)`
+	_, err := r.DB.Exec(query, title)
 	return err
 }
 
 // Get all todos
-func (r *DBRepo) GetTodos() ([]models.Todo, error) {
-	query := `SELECT id, title, description, is_completed FROM todos`
+func (r *TodoRepo) GetTodos() ([]entity.Todo, error) {
+	query := `SELECT id, title, is_completed FROM todos`
 	rows, err := r.DB.Query(query)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-	var todos []models.Todo
+	var todos []entity.Todo
 	for rows.Next() {
-		var todo models.Todo
-		err := rows.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.IsCompleted)
+		var todo entity.Todo
+		err := rows.Scan(&todo.ID, &todo.Title, &todo.IsCompleted)
 		if err != nil {
 			return nil, err
 		}
@@ -61,28 +60,21 @@ func (r *DBRepo) GetTodos() ([]models.Todo, error) {
 }
 
 // Update title of a todo
-func (r *DBRepo) UpdateTodoTitle(id int, title string) error {
+func (r *TodoRepo) UpdateTodoTitle(id int, title string) error {
 	query := `UPDATE todos SET title = $1 WHERE id = $2`
 	_, err := r.DB.Exec(query, title, id)
 	return err
 }
 
-// Update description of a todo
-func (r *DBRepo) UpdateTodoDescription(id int, description string) error {
-	query := `UPDATE todos SET description = $1 WHERE id = $2`
-	_, err := r.DB.Exec(query, description, id)
-	return err
-}
-
 // Update is_completed of a todo
-func (r *DBRepo) UpdateTodoIsCompleted(id int, isCompleted bool) error {
+func (r *TodoRepo) UpdateTodoIsCompleted(id int, isCompleted bool) error {
 	query := `UPDATE todos SET is_completed = $1 WHERE id = $2`
 	_, err := r.DB.Exec(query, isCompleted, id)
 	return err
 }
 
 // Delete a todo
-func (r *DBRepo) DeleteTodo(id int) error {
+func (r *TodoRepo) DeleteTodo(id int) error {
 	query := `DELETE FROM todos WHERE id = $1`
 	_, err := r.DB.Exec(query, id)
 	return err
